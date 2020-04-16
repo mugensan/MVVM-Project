82eb06bbe417b73b34ef2cfb7e50f5a478243035
krodi001 <kev.roditi@gmail.com>
2020-04-16 16:52:55 +0200

    pkg and prep VM for dummy data

---
+++ app/src/main/java/com/example/personal_mvvm/models/Animal.kt
@@ -0,0 +1,5 @@
+package com.example.personal_mvvm.models
+
+data class Animal (
+    val name: String?
+)
---
+++ app/src/main/java/com/example/personal_mvvm/viewmodel/ListViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.personal_mvvm.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.personal_mvvm.models.Animal
+
+//exposing a series of liveData from the backend and our api will need a key
+class ListViewModel(application: Application):AndroidViewModel(application) {
+
+
+    val animals by lazy { MutableLiveData<List<Animal>>() }
+    val loadError by lazy { MutableLiveData<Boolean>() }
+    val loading by lazy { MutableLiveData<Boolean>() }
+
+    //start the retrieval of the information of the backend
+    //Test Mock Data
+    fun refresh(){
+        getAnimals()
+    }
+
+    //dummy data for test
+    private fun getAnimals(){
+        val a1 = Animal("alligator")
+        val a2 = Animal("bee")
+        val a3 = Animal("cat")
+        val a4 = Animal("dog")
+        val a5 = Animal("elephant")
+        val a6 = Animal("flamengo")
+
+        val animalList = arrayListOf(a1,a2,a3,a4,a5,a6)
+        animals.value = animalList
+        loadError.value = false
+        loading.value = false
+    }
+}
