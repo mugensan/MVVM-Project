0bade0dd4e877222bfd07fca9150b0f157b9b526
krodi001 <kev.roditi@gmail.com>
2020-04-16 18:44:12 +0200

    checking functionality with mock data - passed

--- app/src/main/java/com/example/personal_mvvm/view/ListFragment.kt
+++ app/src/main/java/com/example/personal_mvvm/view/ListFragment.kt
@@ -1,17 +1,45 @@
 package com.example.personal_mvvm.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.personal_mvvm.R
+import com.example.personal_mvvm.models.Animal
+import com.example.personal_mvvm.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+    private val listAdapter = AnimalListAdapter(arrayListOf())
+
+
+    private val animalListDataObserver = Observer<List<Animal>> { list ->
+        //if the list is not null perform code block, lambda expression
+        list?.let {
+            rv_animal_list.visibility = View.VISIBLE
+            listAdapter.updateAnimalList(it)
+        }
+    }
+
+    private val loadingLiveDataObserver = Observer<Boolean> { isLoading ->
+        pb_loading_view.visibility = if (isLoading) View.VISIBLE else View.GONE
+        if(isLoading){
+            tv_list_error.visibility = View.GONE
+            rv_animal_list.visibility = View.GONE
+        }
+    }
+
+    private val loadErrorLiveDataObserver = Observer<Boolean> { isError ->
+        tv_list_error.visibility = if(isError) View.VISIBLE else View.GONE
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +49,25 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-    //test btn to move between fragments
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //store the list model and attaches it to the frag
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        //observe the liveData  (ej: MutableLiveData -> viewModel class)
+        viewModel.animals.observe(this, animalListDataObserver)
+        viewModel.loading.observe(this, loadingLiveDataObserver)
+        viewModel.loadError.observe(this, loadErrorLiveDataObserver)
+
+        viewModel.refresh()
+
+        rv_animal_list.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = listAdapter
+        }
+
+        //test btn to move between fragments
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
 //        btn_detail.setOnClickListener{
 //            val action = ListFragmentDirections.actionGoToDetail()
 //            Navigation.findNavController(it).navigate(action)
