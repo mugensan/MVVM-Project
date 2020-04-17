1a83d1145bf0b798004ac2ea92159b5856b4959f
krodi001 <kev.roditi@gmail.com>
2020-04-17 18:03:47 +0200

    refactored for dataBinding

--- app/src/main/java/com/example/personal_mvvm/view/DetailFragment.kt
+++ app/src/main/java/com/example/personal_mvvm/view/DetailFragment.kt
@@ -6,19 +6,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.example.personal_mvvm.R
+import com.example.personal_mvvm.databinding.FragmentDetailBinding
 import com.example.personal_mvvm.models.animal.Animal
 import com.example.personal_mvvm.models.util.getProgressDrawable
 import com.example.personal_mvvm.models.util.loadImage
-import kotlinx.android.synthetic.main.fragment_detail.*
-import kotlinx.android.synthetic.main.items_animal.*
-import kotlinx.android.synthetic.main.items_animal.iv_animal_image
-import kotlinx.android.synthetic.main.items_animal.tv_animal_name
 
 /**
  * A simple [Fragment] subclass.
@@ -26,13 +24,15 @@ import kotlinx.android.synthetic.main.items_animal.tv_animal_name
 class DetailFragment : Fragment() {
 
     var animal: Animal? = null
+    private lateinit var dataBinding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        //dataBinding -Inflate the layout for this fragment
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,28 +45,24 @@ class DetailFragment : Fragment() {
 
         //checking if image has context
         context?.let {
-            iv_animal_image.loadImage(animal?.imageUrl, getProgressDrawable(it))
+            dataBinding.ivAnimalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
         }
-
-        //populating the view
-        tv_animal_name.text = animal?.name
-        tv_animal_location.text = animal?.location
-        tv_animal_lifeSpan.text = animal?.lifeSpan
-        tv_animal_diet.text = animal?.diet
 
         animal?.imageUrl?.let {
             setupBackgroudColor(it)
         }
 
+        //binding info from layout data name
+        dataBinding.animal = animal
     }
 
     //function to set the background in regards to the image spectrum
-    private fun setupBackgroudColor(url:String){
+    private fun setupBackgroudColor(url: String) {
         //use Glide latest functionality april 2020
         Glide.with(this)
             .asBitmap()
             .load(url)
-            .into(object : CustomTarget<Bitmap>(){
+            .into(object : CustomTarget<Bitmap>() {
                 override fun onLoadCleared(placeholder: Drawable?) {
                     TODO("Not yet implemented")
                 }
@@ -74,10 +70,10 @@ class DetailFragment : Fragment() {
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
                     //using palette library
                     Palette.from(resource)
-                        .generate(){palette: Palette? ->
-                            val intColor = palette?.lightMutedSwatch?.rgb ?:0 // if color is null
-                            animal_layout.setBackgroundColor(intColor)
-                    }
+                        .generate() { palette: Palette? ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0 // if color is null
+                            dataBinding.animalLayout.setBackgroundColor(intColor)
+                        }
                 }
 
             })
