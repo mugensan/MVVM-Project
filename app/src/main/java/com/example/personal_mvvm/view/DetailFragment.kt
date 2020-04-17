d23b08d711f4eeefef5c62ed241e2d8215465fbd
krodi001 <kev.roditi@gmail.com>
2020-04-17 17:38:25 +0200

    bg palette for jetpack lib

--- app/src/main/java/com/example/personal_mvvm/view/DetailFragment.kt
+++ app/src/main/java/com/example/personal_mvvm/view/DetailFragment.kt
@@ -1,10 +1,16 @@
 package com.example.personal_mvvm.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.personal_mvvm.R
 import com.example.personal_mvvm.models.animal.Animal
 import com.example.personal_mvvm.models.util.getProgressDrawable
@@ -42,10 +48,39 @@ class DetailFragment : Fragment() {
             iv_animal_image.loadImage(animal?.imageUrl, getProgressDrawable(it))
         }
 
+        //populating the view
         tv_animal_name.text = animal?.name
         tv_animal_location.text = animal?.location
         tv_animal_lifeSpan.text = animal?.lifeSpan
         tv_animal_diet.text = animal?.diet
+
+        animal?.imageUrl?.let {
+            setupBackgroudColor(it)
+        }
+
+    }
+
+    //function to set the background in regards to the image spectrum
+    private fun setupBackgroudColor(url:String){
+        //use Glide latest functionality april 2020
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    //using palette library
+                    Palette.from(resource)
+                        .generate(){palette: Palette? ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?:0 // if color is null
+                            animal_layout.setBackgroundColor(intColor)
+                    }
+                }
+
+            })
     }
 }
 
