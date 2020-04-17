540eebe6b00acc88b7fd2fe902d6c9dc66c6f68f
krodi001 <kev.roditi@gmail.com>
2020-04-17 17:17:12 +0200

    detailScreen ready and receiving

--- app/src/main/java/com/example/personal_mvvm/view/AnimalListAdapter.kt
+++ app/src/main/java/com/example/personal_mvvm/view/AnimalListAdapter.kt
@@ -3,6 +3,7 @@ package com.example.personal_mvvm.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.personal_mvvm.R
 import com.example.personal_mvvm.models.animal.Animal
@@ -34,5 +35,12 @@ class AnimalListAdapter (private val animalList:ArrayList<Animal>): RecyclerView
         //image
         holder.view.iv_animal_image.loadImage(animalList[position].imageUrl,
             getProgressDrawable(holder.view.context))
+
+        //parcelables
+        holder.view.item_animal_layout.setOnClickListener {
+            val action = ListFragmentDirections.actionGoToDetail(animalList[position])
+
+            Navigation.findNavController(holder.view).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/example/personal_mvvm/view/DetailFragment.kt
+++ app/src/main/java/com/example/personal_mvvm/view/DetailFragment.kt
@@ -1,20 +1,25 @@
 package com.example.personal_mvvm.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
+import androidx.fragment.app.Fragment
 import com.example.personal_mvvm.R
+import com.example.personal_mvvm.models.animal.Animal
+import com.example.personal_mvvm.models.util.getProgressDrawable
+import com.example.personal_mvvm.models.util.loadImage
 import kotlinx.android.synthetic.main.fragment_detail.*
-import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.items_animal.*
+import kotlinx.android.synthetic.main.items_animal.iv_animal_image
+import kotlinx.android.synthetic.main.items_animal.tv_animal_name
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
+
+    var animal: Animal? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,10 +31,21 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btn_list.setOnClickListener{
-            val action = DetailFragmentDirections.actionGoToList()
-            Navigation.findNavController(it).navigate(action)
 
+        //retreiving animal info
+        arguments?.let {
+            animal = DetailFragmentArgs.fromBundle(it).animal
         }
+
+        //checking if image has context
+        context?.let {
+            iv_animal_image.loadImage(animal?.imageUrl, getProgressDrawable(it))
+        }
+
+        tv_animal_name.text = animal?.name
+        tv_animal_location.text = animal?.location
+        tv_animal_lifeSpan.text = animal?.lifeSpan
+        tv_animal_diet.text = animal?.diet
     }
 }
+
